8f60642420e0c7068968f209279a2e661906829a
Bobby Powers <bobbypowers@gmail.com>
2016-01-14 23:12:30 -0500

    link against internal_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,10 @@
 #![feature(lang_items)]
-#![feature(start)]
-#![feature(raw)]
 #![feature(libc)]
-#![no_std]
 
 
 extern crate libc;
 
-use core::mem;
+extern crate internal_alloc;
 
 // aligned_alloc
 // calloc
@@ -23,13 +20,3 @@ use core::mem;
 pub extern fn malloc(size: libc::size_t) -> *mut libc::c_void {
     return 0 as *mut libc::c_void;
 }
-
-
-#[lang = "panic_fmt"]
-extern fn panic_fmt(args: &core::fmt::Arguments,
-                    file: &str,
-                    line: u32) -> ! {
-    loop {}
-}
-
-#[lang = "eh_personality"] extern fn eh_personality() {}
