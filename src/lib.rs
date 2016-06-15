8006f83634f66820f3172feb48fddec71af113a6
Bobby Powers <bobbypowers@gmail.com>
2016-06-15 08:47:45 -0700

    remove internal_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,56 @@
 #![feature(lang_items)]
 #![feature(libc)]
+#![feature(link_args)]
 
+#![feature(alloc_jemalloc)]
+
+#![warn(unused_variables)]
+
+extern crate alloc_jemalloc;
 
 extern crate libc;
 
-extern crate internal_alloc;
+use std::sync::{Once, ONCE_INIT};
+use libc::{size_t, c_void};
 
 // aligned_alloc
-// calloc
 // cfree
-// free
-// malloc
-// malloc_usable_size
 // memalign
 // posix_memalign
-// prealloc
+// prealloc ??
+
+//static
+
+static INIT: Once = ONCE_INIT;
+//INIT.call_once(|| {
+//
+//});
 
 #[no_mangle]
-pub extern fn malloc(size: libc::size_t) -> *mut libc::c_void {
+pub extern fn malloc_usable_size(ptr: *mut c_void) -> size_t {
+    println!("malloc_usable_size(_)");
+    return 0;
+}
+
+#[no_mangle]
+pub extern fn malloc(size: size_t) -> *mut c_void {
+    println!("malloc(_)");
     return 0 as *mut libc::c_void;
+}
+
+#[no_mangle]
+pub extern fn calloc(nmemb: size_t, size: size_t) -> *mut c_void {
+    //println!("calloc(_, _)");
+    return 0 as *mut libc::c_void;
+}
+
+#[no_mangle]
+pub extern fn realloc(ptr: *mut c_void, size: size_t) -> *mut c_void {
+    println!("realloc(_, _)");
+    return ptr;
+}
+
+#[no_mangle]
+pub extern fn free(ptr: *mut c_void) {
+    println!("free(_)");
 }
