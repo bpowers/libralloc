b00d23f1efbbba6aab8e0b244c339398d46ff29b
Bobby Powers <bobbypowers@gmail.com>
2016-06-24 09:18:07 -0400

    remove return statements

--- src/lib.rs
+++ src/lib.rs
@@ -27,25 +27,25 @@ static INIT: Once = ONCE_INIT;
 #[no_mangle]
 pub extern fn malloc_usable_size(ptr: *mut c_void) -> size_t {
     println!("malloc_usable_size(_)");
-    return 0;
+    0
 }
 
 #[no_mangle]
 pub extern fn malloc(size: size_t) -> *mut c_void {
     println!("malloc(_)");
-    return 0 as *mut libc::c_void;
+    0 as *mut libc::c_void
 }
 
 #[no_mangle]
 pub extern fn calloc(nmemb: size_t, size: size_t) -> *mut c_void {
     //println!("calloc(_, _)");
-    return 0 as *mut libc::c_void;
+    0 as *mut libc::c_void
 }
 
 #[no_mangle]
 pub extern fn realloc(ptr: *mut c_void, size: size_t) -> *mut c_void {
     println!("realloc(_, _)");
-    return ptr;
+    ptr
 }
 
 #[no_mangle]
